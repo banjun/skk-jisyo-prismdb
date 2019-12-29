532f592a7a5fce13bc07582913ba6feb8ab2ee2c
banjun <banjun@gmail.com>
2019-12-29 23:52:47 +0900

    rough error check in overwriting re-generated jisyo result

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 encoded=$(nkf -WwMQ gen.rq | sed -e 's/=$//g' | tr = % | tr -d '\n')
 result=$(curl -s --fail "https://prismdb.takanakahiko.me/sparql?default-graph-uri=&query=$encoded&format=text%2Fcsv" | tail +3 | sed '$d' | LC_ALL=C sort)
-echo "$result" > SKK-JISYO.prismdb.all.utf8
+echo "$result" | grep ';; okuri-nasi' > /dev/null && echo "$result" > SKK-JISYO.prismdb.all.utf8
