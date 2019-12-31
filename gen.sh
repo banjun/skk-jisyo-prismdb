6b8d4309fd2cc7b08d85587d2334b2d2b353cbdb
banjun <banjun@gmail.com>
2019-12-31 20:27:42 +0900

    add generate query for songs

--- gen.sh
+++ gen.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-encoded=$(nkf -WwMQ gen.rq | sed -e 's/=$//g' | tr = % | tr -d '\n')
-result=$(curl -s --fail "https://prismdb.takanakahiko.me/sparql?default-graph-uri=&query=$encoded&format=text%2Fcsv" | tail +3 | sed '$d' | LC_ALL=C sort)
-echo "$result" | grep ';; okuri-nasi' > /dev/null && echo "$result" > SKK-JISYO.prismdb.all.utf8
+characters_encoded=$(nkf -WwMQ gen-characters.rq | sed -e 's/=$//g' | tr = % | tr -d '\n')
+characters_result=$(curl -s --fail "https://prismdb.takanakahiko.me/sparql?default-graph-uri=&query=$characters_encoded&format=text%2Fcsv" | tail +3 | sed '$d' | LC_ALL=C sort)
+echo "$characters_result" | grep ';; okuri-nasi' > /dev/null && echo "$characters_result" > SKK-JISYO.prismdb.characters.utf8
+
+songs_encoded=$(nkf -WwMQ gen-songs.rq | sed -e 's/=$//g' | tr = % | tr -d '\n')
+songs_result=$(curl -s --fail "https://prismdb.takanakahiko.me/sparql?default-graph-uri=&query=$songs_encoded&format=text%2Fcsv" | tail +3 | sed '$d' | LC_ALL=C sort)
+echo "$songs_result" | grep ';; okuri-nasi' > /dev/null && echo "$songs_result" > SKK-JISYO.prismdb.songs.utf8
+
+# teams_encoded=$(nkf -WwMQ gen-teams.rq | sed -e 's/=$//g' | tr = % | tr -d '\n')
+# teams_result=$(curl -s --fail "https://prismdb.takanakahiko.me/sparql?default-graph-uri=&query=$teams_encoded&format=text%2Fcsv" | tail +3 | sed '$d' | LC_ALL=C sort)
+# echo "$teams_result" | grep ';; okuri-nasi' > /dev/null && echo "$teams_result" > SKK-JISYO.prismdb.teams.utf8
+
+cat SKK-JISYO.prismdb.{characters,songs}.utf8 | LC_ALL=C sort | uniq > SKK-JISYO.prismdb.all.utf8
